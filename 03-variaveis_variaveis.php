db05cf9fc998a726cea2976e039d64bafa13457e
Luiz Omena <luizfilipeyep@gmail.com>
2022-05-23 15:58:10 -0300

    Curso de PHP 7 - Aula 07 - Variáveis dinâmicas

---
+++ 03-variaveis_variaveis.php
@@ -0,0 +1,17 @@
+<?php
+// Variáveis variáveis
+$bebida = "refrigerante";
+
+$$bebida = "Guaraná";
+
+echo $refrigerante;
+
+echo "<br>";
+
+//
+
+$destino = "cidade";
+$$destino = "Ilhéus";
+
+echo $destino;
+echo $cidade;
