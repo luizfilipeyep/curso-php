f42826268549613af648198b1aea6daf65a94188
Luiz Omena <luizfilipeyep@gmail.com>
2022-05-23 15:52:08 -0300

    Curso de PHP 7 - Aula 06 - Variáveis

---
+++ 02-variaveis.php
@@ -0,0 +1,48 @@
+<?php
+// Meus dados
+$nome = "Luiz Filipe";
+$idade = 18;
+$altura = 1.68;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+//Alterando meus dados
+$nome = "Rodrigo Oliveira";
+$idade = 23;
+$altura = 1.73;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+//Alterando meus dados
+$nome = "Felipe Cabral";
+$idade = 29;
+$altura = 1.90;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+// Criando variáveis
+/*
+-Nomes Válidos:
+
+$nomedavariavel
+$_nomedavariavel
+$nomedavariavel01
+$nome_da_variavel
+$nomeDaVariavel
+
+-Nomes Inválidos
+$123456nomedavariavel
+$nome*davariavel$%#
+$nome da variavel
+
+Variáveis são Case Sensitives, ou seja, diferenciam letras minúsculas de maiúsculas
+*/
+
+echo "<hr>";
+
+$carro = "Hilux";
+echo $carro;
+echo $Carro;
+
+$Carro = "Fusca";
+echo $Carro;
